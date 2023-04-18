1d26d5f1251595744c352f764f70402fb672d580
markb <markbuchholz8@gmail.com>
2023-04-18 16:12:14 -0600

    changes

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <fun-components></fun-components>
+  </div>
+</template>
+
+<script>
+import FunComponents from '@/components/FunComponent.vue'
+
+export default {
+  name: 'App',
+  components: {
+    FunComponents,
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/FunComponent.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+<button class="background" @click="color_change"> Click Me</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        methods: {
+            color_change: function () {
+let background = document.querySelector(`.background`)
+background[`style`][`backgroundColor`] = `red`
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
