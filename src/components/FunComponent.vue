3417db0d5b578e86044f2268ad9ed80318e03ad8
markb <markbuchholz8@gmail.com>
2023-04-18 16:47:00 -0600

    fixes

--- src/components/FunComponent.vue
+++ src/components/FunComponent.vue
@@ -1,9 +1,8 @@
 <template>
   <div>
     <button class="background" @click="color_change">Click Me</button>
-    <article v-for="(secret, i) in secrets" :key="i">
-      <p class="pink" @click="pink_back">{{ secret[`string1`]}}</p>
-      <p class="pink_2" @click="pink_back_2">{{ secret[`string2`]}}</p>
+    <article >
+      <p class="pink" @click="pink_back" v-for="(secret, i) in secrets" :key="i">{{ secret }}</p>
     </article>
   </div>
 </template>
@@ -19,18 +18,11 @@ export default {
 let pink_back = document.querySelector(`.pink`);
 pink_back[`style`][`backgroundColor`] = `pink`;
     },
-        pink_back_2: function(){
-let pink_back_2 = document.querySelector(`.pink_2`);
-pink_back_2[`style`][`backgroundColor`] = `pink`;
-    }
   },
   data() {
     return {
-      secrets: [
-        {
-          string1: [`thing 1`, `thing 2`],
-          string2: [`option 1`, `option 2`],
-        },
+      secrets: [[`thing 1`, `thing 2`],[`option 1`, `option 2`],
+
       ],
     };
   },
