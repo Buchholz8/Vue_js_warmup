2cdbfec97b760050ed28d42eeab66e70647a4efb
markb <markbuchholz8@gmail.com>
2023-04-18 16:24:47 -0600

    finisghing touches

--- src/components/FunComponent.vue
+++ src/components/FunComponent.vue
@@ -1,20 +1,37 @@
 <template>
-    <div>
-<button class="background" @click="color_change"> Click Me</button>
-    </div>
+  <div>
+    <button class="background" @click="color_change">Click Me</button>
+    <article v-for="(secret, i) in secrets" :key="i">
+      <p class="pink" @click="pink_back">{{ secret[`string1`]}}</p>
+      <p class="pink" @click="pink_back">{{ secret[`string2`]}}</p>
+    </article>
+  </div>
 </template>
 
 <script>
-    export default {
-        methods: {
-            color_change: function () {
-let background = document.querySelector(`.background`)
-background[`style`][`backgroundColor`] = `red`
-            }
-        }
+export default {
+  methods: {
+    color_change: function () {
+      let background = document.querySelector(`.background`);
+      background[`style`][`backgroundColor`] = `red`;
+    },
+    pink_back: function(){
+let pink_back = document.querySelector(`.pink`);
+pink_back[`style`][`backgroundColor`] = `pink`;
     }
+  },
+  data() {
+    return {
+      secrets: [
+        {
+          string1: [`thing 1`, `thing 2`],
+          string2: [`option 1`, `option 2`],
+        },
+      ],
+    };
+  },
+};
 </script>
 
 <style scoped>
-
 </style>
